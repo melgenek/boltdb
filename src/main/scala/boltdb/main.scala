f9b33767126032d77beddea023e63dae88361a3b
Yevhenii Melnyk <yevhenii.melnyk@snowflake.com>
2022-12-27 16:52:53 +0100

    working foreign memory api example

--- src/main/scala/boltdb/main.scala
+++ src/main/scala/boltdb/main.scala
@@ -1,10 +1,13 @@
+import java.lang.foreign.{MemorySegment, SegmentScope, ValueLayout}
+import java.io.{File, RandomAccessFile}
+import java.nio.channels.FileChannel
+import java.nio.file.{Files, StandardOpenOption, Path, Paths}
+
 @main
 def main(): Unit = {
-  
+  val channel = FileChannel.open(Path.of("test_data/file.txt"), StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE)
+  val segment: MemorySegment = channel.map(FileChannel.MapMode.READ_WRITE, 0, 4096, SegmentScope.global())
 
-
-  println("Hello world!")
+  segment.set(ValueLayout.JAVA_INT, 0, 10)
+  println(segment.get(ValueLayout.JAVA_INT, 0))
 }
-
-
-
